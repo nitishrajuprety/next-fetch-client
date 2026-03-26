702725cbabfe5888217d9ab3a5a5271c25e9cdab
Nitish Raj Uprety <uprety.nitish@gmail.com>
2026-03-26 10:43:28 +0545

    v1.1.4 General optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+    plugins: [dts({rollupTypes: true})],
     build: {
+        minify: 'esbuild',
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'NextFetchClient',
             fileName: (format) => `index.${format}.js`,
             formats: ['es', 'cjs']
         },
-        rollupOptions: {
+        rolldownOptions: {
             external: [], // no deps yet
         },
-        sourcemap: true,
+        sourcemap: false,
         emptyOutDir: true,
     }
 });
